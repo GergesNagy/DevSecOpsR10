2e7d83c29a5f7c1ea3da3aaec4da0347eca8840b
Ahmed Rafat <ahmedrafaat456@example.com>
2025-04-18 15:37:44 +0200

    push docker tasts

---
+++ task2/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>React App in Docker</h1>
+      <p>Built with multi-stage Dockerfile</p>
+    </div>
+  );
+}
+
+export default App;
